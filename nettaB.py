f7b2a6c044e14d9f90a1540a0672bdaeff3859b0
Chatrumor <mel223210@gmail.com>
2018-05-23 18:28:29 -0500

    Fix styling.
    Fix styling to conform to PEP-8 specifications.

--- nettaB.py
+++ nettaB.py
@@ -4,7 +4,9 @@
 #
 # Use for NotEvil purposes only.
 
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''nettaB: Python Library for Communicating with Edit the Text.'''
 import html
 try:
     import requests
@@ -12,40 +14,53 @@ except ImportError:
     print("Requests liblary not detected! Visit the Netta-B wiki for information on how to install it.")
     exit()
 
+
 def Save(page="", text="", printOutput=True):
+    '''The Save command can be used to submit text to a page.'''
     requests.post("http://htwins.net/edit/" + page, data={'content': text})
     if printOutput:
         print(text)
 
+
 def Read(page=""):
+    '''The Read command can be used to get the contents of a page.'''
     r = requests.get("http://htwins.net/edit/" + page)
     r.encoding = "utf8"
     read = r.text
-    r = ((read.split('<textarea rows="20" cols="100" name="content">'))[1].split('</textarea>')[0])
+    r = ((read.split('<textarea rows="20" cols="100" name="content">'))
+         [1].split('</textarea>')[0])
     return html.unescape(r)
-    
+
 
 def GetIP(page=""):
+    '''The GetIP command can be used to see what IP Address last edited a page.'''
     r = requests.get("http://htwins.net/edit/" + page)
     read = r.text
-    return((read.split('<dd id="ip">'))[1].split('</dd>')[0])
+    return read.split('<dd id="ip">')[1].split('</dd>')[0]
+
 
 def Copy(page="", to="", printOutput=True):
+    '''The Copy command can be used to copy the contents of a page to another page.'''
     Save(to, Read(page), False)
     if printOutput:
         print("Copied {} to {}.".format(page, to))
 
+
 def ReadLine(page="", line=0, notFound=""):
+    '''The ReadLine command can be used to get one line of a page.'''
     output = Read(page)
     try:
         return output.splitlines()[line]
     except IndexError:
         return notFound
 
+
 def Append(page="", text="", printOutput=True):
+    '''The Append command can be used to append text to a existing page's contentd.'''
     Save(page, Read(page) + "\n" + text, False)
     if printOutput:
         print("Appended {} to {}.".format(text, page))
-      
-if "__main__" == __name__:
+
+
+if __name__ == '__main__':
     print("Great! Everything is working. Now you can import Netta-B into your Python projects by using 'import nettaB'.")
